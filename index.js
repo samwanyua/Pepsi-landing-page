740c18c191bc0472deea3da7ed6da56074712001
samwanyua <samexample8@gmail.com>
2024-01-08 16:33:17 +0300

    JavaScript file

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function imgSlider(stuff){
+    document.querySelector('.pepsi').src = stuff;
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.backgroundColor = color; 
+}
